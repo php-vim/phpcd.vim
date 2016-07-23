php-vim/phpcd.vim

commit 2d66467f68eaab6763d48fa8a1924bc80633ca98

there is no need to rely on the composer class_loader

php/ClassInfo/ComposerClassmapFileRepository.php
@@ -16,8 +16,6 @@ class ComposerClassmapFileRepository implements ClassInfoRepository
 
     private $project_root;
 
-    private $classLoader;
-
     private $classmap = [];
 
     /** @var ClassInfoFactory **/
@@ -35,14 +33,12 @@ class ComposerClassmapFileRepository implements ClassInfoRepository
 
     public function __construct(
         $project_root,
-        ClassLoader $classLoader,
         PatternMatcher $pattern_matcher,
         ClassInfoFactory $classInfoFactory,
         PHPFileInfoFactory $fileInfoFactory,
         LoggerInterface $logger
     ) {
         $this->pattern_matcher = $pattern_matcher;
-        $this->classLoader = $classLoader;
         $this->classInfoFactory = $classInfoFactory;
         $this->fileInfoFactory = $fileInfoFactory;
         $this->setLogger($logger);
@@ -104,13 +100,18 @@ class ComposerClassmapFileRepository implements ClassInfoRepository
      */
     private function isValid($classpath)
     {
-        $filePath = $this->classLoader->findFile($classpath);
+        try {
+            $reflection = new \ReflectionClass($classpath);
+            $file_path = $reflection->getFileName();
+            $file_info = $this->file_infoFactory->createFileInfo($file_path);
+        } catch (\ReflectionException $e) {
+            $this->logger->warning($e->getMessage());
+            return false;
+        }
 
-        $fileInfo = $this->fileInfoFactory->createFileInfo($filePath);
-
-        if ($fileInfo->hasErrors()) {
+        if ($file_info->hasErrors()) {
             $message = 'Class %s did not passed validation and then cannot be added to class info repository. Reason:';
-            $this->logger->warning(sprintf($message, $classpath), $fileInfo->getErrors());
+            $this->logger->warning(sprintf($message, $classpath), $file_info->getErrors());
             return false;
         } else {
             return true;

php/main.php
@@ -14,7 +14,7 @@ require __DIR__ . '/../vendor/autoload.php';
 /** load autoloader for the project **/
 $composer_autoload_file = $root . '/vendor/autoload.php';
 if (is_readable($composer_autoload_file)) {
-    $class_loader = require $composer_autoload_file;
+    require $composer_autoload_file;
 }
 
 use PHPCD\Factory as F;
@@ -32,7 +32,7 @@ if ($handler_name == 'PHPCD') {
     $handler = new PHPCD\PHPCD($root, $logger,
         $class_info_factory, $phpfile_info_factory);
 } else {
-    $ccfr = new ComposerClassmapFileRepository($root, $class_loader, $matcher,
+    $ccfr = new ComposerClassmapFileRepository($root, $matcher,
         $class_info_factory, $phpfile_info_factory, $logger);
     $handler = new PHPCD\PHPID($root, $logger, $ccfr);
 }

commit eb9a050958e62ad71ad901c36c6c9427a15b5056

Added test for ComposerClassmapFileRepositoryTest->find()
In the current form this test has to show regression
that came with 2d66467f.

tests/php/ClassInfoRepository/ComposerClassmapFileRepositoryTest.php
@@ -1,0 +1,34 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PHPCD\PatternMatcher\SubsequencePatternMatcher;
+use Psr\Log\LoggerInterface;
+use PHPCD\PHPFileInfo\PHPFileInfoFactory;
+use PHPCD\ClassInfo\ClassInfoFactory;
+use PHPCD\ClassInfo\ComposerClassmapFileRepository;
+
+class ComposerClassmapFileRepositoryTest extends TestCase
+{
+    public function testXxx()
+    {
+        $pattern_matcher = $this->createMock(SubsequencePatternMatcher::class);
+        $pattern_matcher->method('match')->will($this->returnArgument(true));
+
+        $project_root =  dirname(__FILE__).'/../Fixtures/ClassInfoRepository/Fakeroot/ExampleWithNonExistingSuperclass';
+
+        $classInfoFactory = new ClassInfoFactory($pattern_matcher);
+
+        $fileInfoFactory = $this->createMock(PHPFileInfoFactory::class);
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $repository = new ComposerClassmapFileRepository(
+            $project_root,
+            $pattern_matcher,
+            $classInfoFactory,
+            $fileInfoFactory,
+            $logger
+        );
+
+        $repository->find('PHPCD\\Fixtures\\StringBasedPHPFileInfo\\A');
+    }
+}

tests/php/Fixtures/ClassInfoRepository/Fakeroot/ExampleWithNonExistingSuperclass/vendor/composer/autoload_classmap.php
@@ -1,0 +1,7 @@
+<?php
+
+$baseDir = dirname(__FILE__, 9);
+
+return [
+    'PHPCD\\Fixtures\\StringBasedPHPFileInfo\\A' => $baseDir . '/tests/php/Fixtures/StringBasedPHPFileInfo/ExampleWithNonExistingSuperclass.php',
+];